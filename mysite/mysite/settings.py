c01843cf682b803559370ab00814c094bd51ef4f
friz25 <friz25@gmail.com>
2025-02-13 11:23:49 +0200

    1.5 + Автотесты Selenium в Dj

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -78,9 +78,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+        "TEST": {
+            "NAME": BASE_DIR / "db.sqlite3",
+        },
     }
 }
 
